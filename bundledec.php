4ca2e1e475953a65c52b9e3df2471d31b70d94ea
Lucian Villaseñor <lucianv_001@yahoo.com>
2017-08-26 23:11:07 -0500

    Add files via upload

---
+++ bundledec.php
@@ -0,0 +1,30 @@
+<?php
+// Listen for the POST request 
+$payload = json_decode(file_get_contents('php://input'));
+
+
+
+//What we want the webhook to do 
+//The URL that we want to send a PUT request to.
+$url = 'https://store-njwk9edm.mybigcommerce.com/api/v2/products/20961';
+$fields = json_encode(array("description" => "$payload"));
+$headers = array(
+	'Content-type: application/json',
+    'Authorization: Basic '. base64_encode("lucian:6b02cfb11c15b43b564106e3debccfcf1b41a625")
+);
+
+//Initiate cURL
+$ch = curl_init($url);
+//Use the CURLOPT_PUT option to tell cURL that
+//this is a PUT request.
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+//Execute the request.
+$return = curl_exec($ch);
+curl_close($ch);
+
+
+?>
